b3bc206573b23dc7e1ed48c0e438b0e74d4d5b1e
BuildTools <unconfigured@null.spigotmc.org>
2023-08-02 12:53:36 +0200

    no finish

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 from player import Player
+from monster import Monster
 
 
 class Game:
@@ -7,6 +8,11 @@ class Game:
     def __init__(self):
         self.player = Player()
         self.pressed = {}
+        self.all_monsters = pygame.sprite.Group
         self.score = 0
         self.projectile_boost = True
-#
+        self.spawn_monster()
+
+    def spawn_monster(self):
+        monster = Monster()
+        self.all_monsters.add(monster)
---
+++ monster.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+class Monster(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 5
+        self.image = pygame.image.load('assets/mummy.png')
+        self.rect = self.image.get_rect()
